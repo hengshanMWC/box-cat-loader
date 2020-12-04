95fb2bc7f2007620cbcd4bb7ed89aa42a22cc2f2
mawencan <1819293933@qq.com>
2020-12-04 18:29:29 +0800

    fix: 依赖缓存和路径问题

--- test/build/webpack.index.js
+++ test/build/webpack.index.js
@@ -11,7 +11,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js/,
+        test: /\.js$/,
         include: [
           path.resolve(__dirname, ITEM_PATH, 'index.js')
         ],

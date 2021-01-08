6c490a70f7de77f6a30ad1e9fb932a10e2b5a91c
mawencan <1819293933@qq.com>
2021-01-08 14:39:53 +0800

    test: 增加例子

--- test/apis/index/index.js
+++ test/apis/index/index.js
@@ -1,5 +1,15 @@
 import { createProxy } from 'box-cat'
 import response from 'test/response'
 import values from 'test/apis/index/data'
+// import commodity from './commodity'
+// import comment from './comment'
+// import user from './user'
+// const values =  {
+//   ...commodity,
+//   ...user,
+//   ...comment,
+//   postOrder: 'order',
+//   getOrder: 'order/:id'
+// }
 const https = createProxy(values, response)
 export default https
